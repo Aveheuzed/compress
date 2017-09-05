70b036986ea03b17ad243b67b8fdd97b6b7ca080
Aveheuzed <a.masson555@ntymail.com>
2017-09-05 19:24:19 +0200

    Launching a new project, i.e. compression of python scripts

---
+++ pyfile/__main__.py
@@ -0,0 +1,14 @@
+#!/usr/bin/env python3
+
+""""The goal of that script is to compress a python3 script the most it can,
+and then to expand it so that it runs the same.
+Assertions, comments, docstrings, additionnal '\\n'  are to be saved, but the expanded version
+should differ from the original one by its respect of PEP 8 :
+https://openclassrooms.com/courses/apprenez-a-programmer-en-python/de-bonnes-pratiques#/id/r-235217
+"""
+
+def compress(text):
+        pass
+
+def expand(text):
+        pass
